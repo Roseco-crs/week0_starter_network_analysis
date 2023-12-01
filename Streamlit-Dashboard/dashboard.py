92f4613c66ad762a00f40d2a75d8a26ae9f9ddf0
Rodolphe Segbedji <rodolphe.segbedji@aims.ac.rw>
2023-12-01 17:53:10 +0100

    updated streamlit first app

--- Streamlit-Dashboard/dashboard.py
+++ Streamlit-Dashboard/dashboard.py
@@ -1,33 +1,31 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from src.loader import SlackDataLoader
-import src.loader as loader
-import src.utils as utils
+# from src.loader import SlackDataLoader
+# import src.loader as loader
+#import src.utils as utils
 import os, sys
+import matplotlib.pyplot as plt
+import plotly.express as px
 
+@st.cache
+def load_data():
+    pp = os.path.abspath('../Streamlit-Dashboard/dataf.csv')
+    df2 = pd.read_csv(pp)
+    return df2
 
-p = os.path.abspath('../Streamlit-Dashboard/data2.csv')
-pp = os.path.abspath('../Streamlit-Dashboard/dataf.csv')
-
+# p = os.path.abspath('../Streamlit-Dashboard/data2.csv')
+# pp = os.path.abspath('../Streamlit-Dashboard/dataf.csv')
 
 st.markdown('# Slack Message Analysis')
 st.markdown('#### Project Description')
 st.markdown("The purpose of this analysis is to understand data, answer usefull questioons")
 
-st.markdown("#### Load Data-1")
-df1 = pd.read_csv(p)
-st.write(df1)
+dataa = load_data()
+st.write(dataa)
 
-st.markdown("#### Load Data-2")
+st.markdown('#### Histogramme of essage sender name')
+fig = px.histogram(dataa, y='sender_name', color=None)
 
-df2 = pd.read_csv(pp)
-st.write(df2)
-
-
-
-
-
-
-
+st.plotly_chart(fig)
 
